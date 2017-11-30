9b799757ba706572afaf89696382ad9e1853c10d
DOMAIN\AShevchenko <alexander.shevchenko@firstlinesoftware.com>
2017-11-30 13:34:20 +0300

    First test is added

---
+++ AutoTestsProject/AutoTestProject/TestBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutoTestsProject
+{
+    [TestFixture]
+    public abstract class TestBase
+    {
+        protected IWebDriver Driver;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            Driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void TestCleanup()
+        {
+            Driver.Quit();
+        }
+    }
+}
